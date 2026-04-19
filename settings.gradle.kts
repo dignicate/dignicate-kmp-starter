f27269a917c65bf5bafe45655ae1989676095ecd
xinyiqiw <shinichi_watanabe@dignicate.com>
2026-04-19 21:02:26 +0900

    Set up initial project structure with Gradle configuration and .gitignore

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "dignicate-kmp-starter"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
